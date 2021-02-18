19ab115c725353281d7cdb5a69c32bd6413b72da
Raynara <raynaralia17@gmail.com>
2021-02-18 16:32:41 -0300

    tela incial

--- app/src/main/java/com/example/takedelivery/AcessoActivity.java
+++ app/src/main/java/com/example/takedelivery/AcessoActivity.java
@@ -7,17 +7,19 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AcessoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_acesso);
 
         getWindow().setStatusBarColor(Color.parseColor("#FDD54A0A"));
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FDEA520C")));
         actionbar.setTitle("Take Delivery");
+
+
     }
 }
